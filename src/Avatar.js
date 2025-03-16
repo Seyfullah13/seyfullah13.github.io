202d496471198fd751335c3275d518c630863f58
Seyfullah <108519166+Seyfullah13@users.noreply.github.com>
2025-03-16 14:26:46 +0100

    Removed old profile photo, updated with new one, and added 13vh top margin

--- src/Avatar.js
+++ src/Avatar.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Badge, Box, Stack, Avatar } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import avatarIcon from "./images/PhotoSeyf.jpg"; 
+import avatarIcon from "./images/Seyfullah.png"; 
+import AvatarBackground from "./images/tech.png";
+import'./index.css';
 
 // Styled Avatar avec MUI v5
 const StyledAvatar = styled(Avatar)({
@@ -14,23 +16,36 @@ const StyledAvatar = styled(Avatar)({
 
 const AvatarCustom = ({ children, ...props }) => {
     return (
-        <Stack flexDirection="row" >
-            <Box sx={{ background: (theme) => theme.palette.background.default, borderRadius: 3, padding: 2, margin: 2 }}> 
-                <Badge
-                    overlap="rectangle"
-                    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                    variant="dot"
-                    sx={{ border: "0px solid white", width: "fit-content" }}
-                >
-                    <StyledAvatar alt="Remy Sharp" src={avatarIcon} id="avatar" />
-                </Badge>
-            </Box>
-        </Stack>
+        <div 
+          className="container-fluid "
+          style={{
+            backgroundImage: `url(${AvatarBackground})`,
+            backgroundSize: 'cover',
+            minHeight: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+         
+         
+          }}
+        >
+            <Stack flexDirection="column"  className='mx-2'>
+                <Box sx={{ borderRadius: 3, padding: 1, margin: 1 ,  }}> 
+                    <Badge
+                        overlap="circular"
+                        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                        variant="dot"
+                        sx={{ border: "0px solid white", width: "fit-content" }}
+                    >
+                        <StyledAvatar alt="Remy Sharp" src={avatarIcon} id="avatar" />
+                    </Badge>
+                </Box>
+            </Stack>
+        </div>
     );
 }
+
 AvatarCustom.propTypes = {
     children: PropTypes.node,
 };
-
 
 export default AvatarCustom;
