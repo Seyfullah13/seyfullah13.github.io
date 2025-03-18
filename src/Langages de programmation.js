d869963913518213ae1464bbf3bf0a7435850b39
Seyfullah <108519166+Seyfullah13@users.noreply.github.com>
2025-03-18 15:58:51 +0100

    Replaced old logos with those from the Figma design Adjusted text positioning for better alignment Added CSS to refine the placement of the "Programming Languages" title Fixed typos in translation.json (French)

--- src/Langages de programmation.js
+++ src/Langages de programmation.js
@@ -1,25 +1,36 @@
-import 'animate.css/animate.min.css';
-import { motion } from 'framer-motion';
+
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import SkillsBackground from './images/tech.png';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import logohtml from './images/html-5.png'
+
 
 function LangageDeProgrammation() {
   const { t } = useTranslation();
 
   return (
-    <motion.div className="container-fluid"
+    <div className="container-fluid"
       style={{ backgroundImage: `url(${SkillsBackground})`, backgroundSize: 'cover', minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-      <motion.h1
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, type: "linear" }} // Aligné avec Portfolio
-        className="text-center bg-white p-2 d-inline-block mx-auto mb-3"
+      <h1
+        
+        className="text-center bg-white p-2 d-inline-block mb-auto programming"
         style={{ backgroundColor: 'rgba(255, 255, 255, 0.8)' }}
       >
         {t("skills")}
-      </motion.h1>
-    </motion.div>
+      </h1>
+      <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src={logohtml} alt='HTML5' />
+      <Card.Body>
+        <Card.Title>HTML</Card.Title>
+        <Card.Text>
+        HTML est un langage de balisage structurant le contenu des pages web.
+        </Card.Text>
+        <Button variant="primary">Documentation</Button>
+      </Card.Body>
+    </Card>
+    </div>
   );
 }
 
